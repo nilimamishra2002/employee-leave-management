b65b6f8a754649564ad70511d90d2f0e61dad196
nilimamishra2002 <nilimamishra2002@gmail.com>
2026-01-04 18:23:23 +0530

    frontend changes

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // React Router fallback
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(
     path.join(__dirname, "../frontend/dist/index.html")
   );
